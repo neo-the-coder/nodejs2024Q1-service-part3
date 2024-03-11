f32f5de4d8f39210e49dd60331e1054d53ad7fa0
neo-the-coder <kamranz150@gmail.com>
2024-03-12 01:59:37 +0400

    add: OpenAPI file, .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './domains/user/user.module';
 import { TrackModule } from './domains/track/track.module';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ArtistModule } from './domains/artist/artist.module';
 import { AlbumModule } from './domains/album/album.module';
 import { FavoriteModule } from './domains/favs/favs.module';
 
 @Module({
-  imports: [UserModule, TrackModule, ArtistModule, AlbumModule, FavoriteModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    UserModule,
+    TrackModule,
+    ArtistModule,
+    AlbumModule,
+    FavoriteModule,
+  ],
 })
 export class AppModule {}
